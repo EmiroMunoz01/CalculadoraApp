150173b839b737fbd1657dfdaaa0f6af2668aad0
EmiroMunoz01 <emiro_2001@icloud.com>
2024-12-04 11:15:40 -0500

    Proyecto Concreto

---
+++ Backend/src/main/java/com/calculadora/calculadora/Modelo/Concreto.java
@@ -0,0 +1,19 @@
+package com.calculadora.calculadora.Modelo;
+
+import jakarta.persistence.Entity;
+
+@Entity
+
+public class Concreto {
+  
+    private Integer idConcreto;
+    
+    private double ancho;
+    private double largo;
+    private double grosor;
+    private double calculoConcreto;
+    private String fechaCreacion;
+
+    
+
+}
